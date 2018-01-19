83bff915523975ddeb92b9340ccfca1a31a87b5e
David Willie <david.willie.1@gmail.com>
2018-01-20 10:13:13 +1100

    remove declaration in loop, simplify

--- pr-check.js
+++ pr-check.js
@@ -2,6 +2,5 @@ const elems = document.getElementsByClassName("blob-code-addition");
 const unicodeRegex = /([^\x00-\x7F])+/g;
 
 for (var i = 0; i < elems.length; i++) {
-    const e = elems[i];
-    e.innerHTML = e.innerHTML.replace(unicodeRegex, "<span style=\"background-color: red;\">$&</span>"); 
+    elems[i].innerHTML = elems[i].innerHTML.replace(unicodeRegex, "<span style=\"background-color: red;\">$&</span>"); 
 }
