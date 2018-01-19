f623bc0c08a63dfdad2680918566582addc07fd5
David Willie <david.willie.1@gmail.com>
2018-01-20 10:04:44 +1100

    Make it work with chrome

--- pr-check.js
+++ pr-check.js
@@ -1,6 +1,7 @@
 const elems = document.getElementsByClassName("blob-code-addition");
 const unicodeRegex = /([^\x00-\x7F])+/g;
 
-Array.forEach(elems, e => { 
+for (var i = 0; i < elems.length; i++) {
+    const e = elems[i];
     e.innerHTML = e.innerHTML.replace(unicodeRegex, "<span style=\"background-color: red;\">$&</span>"); 
-});
+}
