5e1d2b29bc70e5cf3faf3e1d001d26ee3c8ed744
David Willie <david.willie.1@gmail.com>
2018-01-20 12:44:59 +1100

    Make sure zero width characters show up

--- pr-check.js
+++ pr-check.js
@@ -2,5 +2,5 @@ const elems = document.getElementsByClassName("blob-code-addition");
 const unicodeRegex = /([^\x00-\x7F])+/g;
 
 for (var i = 0; i < elems.length; i++) {
-    elems[i].innerHTML = elems[i].innerHTML.replace(unicodeRegex, "<span style=\"background-color: red;\">$&</span>"); 
+    elems[i].innerHTML = elems[i].innerHTML.replace(unicodeRegex, "<span style=\"min-width: 3px; background-color: red;\">$&</span>"); 
 }
